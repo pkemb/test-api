86c3b9b49796a9a08921c3bd9de3fb4a6d661533
pkemb <pkemb@outlook.com>
2023-09-07 22:41:21 +0800

    增加 -nostartfiles 选项
    Signed-off-by: pkemb <pkemb@outlook.com>
    Change-Id: I20fbfb6dcb777ffa52bbc380a20f9b317d7caf1e

--- submk/nomain.mk
+++ submk/nomain.mk
@@ -1,12 +1,15 @@
-# 针对指定目标修改变量
-nomain: LDFLAGS := -static -e nomain -lc
+# # 针对指定目标修改变量
+# nomain: LDFLAGS := -static -e nomain -lc
 
-# 增加文件到清理列表
-clean_files += nomain.o
+# # 增加文件到清理列表
+# clean_files += nomain.o
 
-# 重新定义规则
-nomain.o: $(srcdir)/nomain.c
-	$(CC) $(CFLAGS) -c $< -o $@
+# # 重新定义规则
+# nomain.o: $(srcdir)/nomain.c
+# 	$(CC) $(CFLAGS) -c $< -o $@
 
-nomain: nomain.o
-	$(LD) $< -o $@ $(LDFLAGS)
+# nomain: nomain.o
+# 	$(LD) $< -o $@ $(LDFLAGS)
+
+# 或者使用 -nostartfiles 选项，不链接 ctr1.o。这样可以一步完成编译链接，不必拆开编译和链接。
+nomain: CFLAGS += -nostartfiles -static -e nomain -lc
